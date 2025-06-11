5458fad629e27465cfefac5f035d12fc855f052b
Efrain <efrain.sc89@outlook.com>
2025-06-11 16:03:06 -0600

    Removed trading if no account 2

--- src/app/components/Token.js
+++ src/app/components/Token.js
@@ -6,33 +6,22 @@ import Image from 'next/image';
 function Token({ token, account }) {
 
   const openInNewTab = () => {
-    const tokenFid = token.fId
-    localStorage.setItem('tradeData', JSON.stringify({tokenFid}));
-    window.open('/Trade', '_blank', 'noopener,noreferrer');
+    if (account) {
+      const tokenFid = token.fId
+      localStorage.setItem('tradeData', JSON.stringify({tokenFid}));
+      window.open('/Trade', '_blank', 'noopener,noreferrer');
+    }
   };
 
   return (
-      <div>
-        {!account ? (
-          <button className="token">
-            <div className="token__details">
-              <Image src={token.image} alt="token image" width={200} height={200} />
-              <p>created by {token.creator.slice(0, 6) + '...' + token.creator.slice(38, 42)}</p>
-              <p>Market Cap: {ethers.formatUnits(token.raised, 18)} ETH</p>
-              {token.name}
-            </div>
-          </button>
-        ):(
-          <button onClick={openInNewTab} className="token">
-            <div className="token__details">
-              <Image src={token.image} alt="token image" width={200} height={200} />
-              <p>created by {token.creator.slice(0, 6) + '...' + token.creator.slice(38, 42)}</p>
-              <p>Market Cap: {ethers.formatUnits(token.raised, 18)} ETH</p>
-              {token.name}
-            </div>
-          </button>
-        )}
-      </div>     
+    <button onClick={openInNewTab} className="token">
+      <div className="token__details">
+        <Image src={token.image} alt="token image" width={200} height={200} />
+        <p>created by {token.creator.slice(0, 6) + '...' + token.creator.slice(38, 42)}</p>
+        <p>Market Cap: {ethers.formatUnits(token.raised, 18)} ETH</p>
+        {token.name}
+      </div>
+    </button>   
   );
 }
 
