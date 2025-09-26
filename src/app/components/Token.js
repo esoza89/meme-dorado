bcfbacd6508ed8ff9ce76bbff0dc950cdac88a66
Efrain <efrain.sc89@outlook.com>
2025-09-25 18:55:17 -0600

    internationalization token details

--- src/app/components/Token.js
+++ src/app/components/Token.js
@@ -3,8 +3,10 @@
 import { ethers } from "ethers";
 import Image from 'next/image';
 import config from '../config.json';
+import {useTranslations} from 'next-intl';
 
 function Token({ token, account, chainId }) {
+  const t = useTranslations('tokenDetails');
 
   const openInNewTab = () => {
     let key = Number(Object.keys(config)[0]);
@@ -19,8 +21,10 @@ function Token({ token, account, chainId }) {
     <button onClick={openInNewTab} className="token">
       <div className="token__details">
         <Image src={token.image} alt="token image" width={400} />
-        <p>created by {token.creator.slice(0, 6) + '...' + token.creator.slice(38, 42)}</p>
-        <p>Market Cap: {ethers.formatUnits(token.raised, 18)} ETH</p>
+        {/* eslint-disable-next-line react/no-unescaped-entities */}
+        <p>{t('created') + token.creator.slice(0, 6) + '...' + token.creator.slice(38, 42)}</p>
+        {/* eslint-disable-next-line react/no-unescaped-entities */}
+        <p>{t('cap') + ethers.formatUnits(token.raised, 18)} ETH</p>
         {token.name}
       </div>
     </button>   
